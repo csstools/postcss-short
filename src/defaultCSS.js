4bf8c2148a5d0575251d6528b273e18b54901034
Jonathan Neal <jonathantneal@hotmail.com>
2017-01-25 01:17:04 -0500

    gh-pages

---
+++ src/defaultCSS.js
@@ -0,0 +1,16 @@
+module.exports = `.banner {
+	color: #abccfc #212231;
+	font-size: 1.5em/2;
+	font-weight: light;
+	position: fixed 0 0 *;
+	size: 100% 48px;
+}
+
+.section {
+	border-top-radius: 50px;
+	margin: 40px;
+}
+
+.section.inset {
+	margin: * auto;
+}`;
